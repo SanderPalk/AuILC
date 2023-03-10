65d32d77e4a4d212a4714eeacc41fd8f63750d26
sanderpalk <sanderpalk0@gmail.com>
2023-03-10 12:33:52 +0200

    Fixed compile errors

--- src/components/StockLoop.js
+++ src/components/StockLoop.js
@@ -5,20 +5,20 @@ function StockLoop() {
         <div className="stock-loop">
             <div className="stock-loop-container animation">
                 <div className="stock-loop-array">
-                    <div className="list-item"><span className='stock-name'> LCB:</span> +2.47%<img alt='' className='arrow-up'/> </div>
-                    <div className="list-item"><span className='stock-name'>LCGS:</span> +1.07%<img alt='' className='arrow-up'/></div>
-                    <div className="list-item"><span className='stock-name'>LCTC:</span> -2.03%<img alt='' className='arrow-down'/></div>
-                    <div className="list-item"><span className='stock-name'>GCD:</span> +29.39%<img alt='' className='arrow-up'/></div>
-                    <div className="list-item"><span className='stock-name'>LCNC:</span> -6.77%<img alt='' className='arrow-down'/></div>
-                    <div className="list-item"><span className='stock-name'>GMLC:</span> +13.92<img alt='' className='arrow-up'/></div>
+                    <div className="list-item"><span className='stock-name'> LCB:</span> +2.47%<img alt=' ' className='arrow-up'/> </div>
+                    <div className="list-item"><span className='stock-name'>LCGS:</span> +1.07%<img alt=' ' className='arrow-up'/></div>
+                    <div className="list-item"><span className='stock-name'>LCTC:</span> -2.03%<img alt=' ' className='arrow-down'/></div>
+                    <div className="list-item"><span className='stock-name'>GCD:</span> +29.39%<img alt=' ' className='arrow-up'/></div>
+                    <div className="list-item"><span className='stock-name'>LCNC:</span> -6.77%<img alt=' ' className='arrow-down'/></div>
+                    <div className="list-item"><span className='stock-name'>GMLC:</span> +13.92<img alt=' ' className='arrow-up'/></div>
                 </div>
                 <div className="stock-loop-array">
-                    <div className="list-item"><span className='stock-name'> LCB:</span> +2.47%<img alt='' className='arrow-up'/> </div>
-                    <div className="list-item"><span className='stock-name'>LCGS:</span> +1.07%<img alt='' className='arrow-up'/></div>
-                    <div className="list-item"><span className='stock-name'>LCTC:</span> -2.03%<img alt='' className='arrow-down'/></div>
-                    <div className="list-item"><span className='stock-name'>GCD:</span> +29.39%<img alt='' className='arrow-up'/></div>
-                    <div className="list-item"><span className='stock-name'>LCNC:</span> -6.77%<img alt='' className='arrow-down'/></div>
-                    <div className="list-item"><span className='stock-name'>GMLC:</span> +13.92<img alt='' className='arrow-up'/></div>
+                    <div className="list-item"><span className='stock-name'> LCB:</span> +2.47%<img alt=' ' className='arrow-up'/> </div>
+                    <div className="list-item"><span className='stock-name'>LCGS:</span> +1.07%<img alt=' ' className='arrow-up'/></div>
+                    <div className="list-item"><span className='stock-name'>LCTC:</span> -2.03%<img alt=' ' className='arrow-down'/></div>
+                    <div className="list-item"><span className='stock-name'>GCD:</span> +29.39%<img alt=' ' className='arrow-up'/></div>
+                    <div className="list-item"><span className='stock-name'>LCNC:</span> -6.77%<img alt=' ' className='arrow-down'/></div>
+                    <div className="list-item"><span className='stock-name'>GMLC:</span> +13.92<img alt=' ' className='arrow-up'/></div>
                 </div>
             </div>
         </div>
